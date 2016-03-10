08401ce4b2b2e7c3b25c208078edc8b39be78921
Tomas Zaruba <tomas.zaruba@byzance.cz>
2016-03-10 19:53:05 +0100

    Version 1.05.2  (Fix add WS_Homer)

---
+++ app/utilities/webSocket/developing/WS_Homer_Cloud.java
@@ -0,0 +1,20 @@
+package utilities.webSocket.developing;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class WS_Homer_Cloud extends WebSCType {
+
+    String server_name;
+    String instance_name;
+
+
+    @Override
+    public void onClose() {
+
+    }
+
+    @Override
+    public void onMessage(JsonNode json) {
+
+    }
+}
