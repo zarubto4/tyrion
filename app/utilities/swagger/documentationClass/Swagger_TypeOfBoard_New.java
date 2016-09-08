bae50995e212da81dc4d053c741b9ae8f7a15c89
Tomas Zaruba <tomas.zaruba@byzance.cz>
2016-09-08 13:08:04 +0200

    Oprava TypeOfBoards

--- app/utilities/swagger/documentationClass/Swagger_TypeOfBoard_New.java
+++ app/utilities/swagger/documentationClass/Swagger_TypeOfBoard_New.java
@@ -15,9 +15,15 @@ public class Swagger_TypeOfBoard_New {
     public String name;
 
     @Constraints.Required
-    @Constraints.MinLength(value = 4, message = "The description must have at least 4 characters")
-    @Constraints.MaxLength(value = 255, message = "The description must not have more than 255 characters.")
-    @ApiModelProperty(required = true, value = "Length must be between 4 and 255 characters.")
+    @Constraints.MinLength(value = 4)
+    @Constraints.MaxLength(value = 60, message = "The name must not have more than 60 characters.")
+    @ApiModelProperty(required = true, value = "The description must have at least 4 characters")
+    public String compiler_target_name;
+
+    @Constraints.Required
+    @Constraints.MinLength(value = 4)
+    @Constraints.MaxLength(value = 255, message = "The name must not have more than 255 characters.")
+    @ApiModelProperty(required = true, value = "The description must have at least 4 characters")
     public String description;
 
     @Constraints.Required
