0c7bdad3d9f1a45f7c5e50c0083cd4dc9f03d077
Tomas Zaruba <tomas.zaruba@byzance.cz>
2017-02-07 12:43:52 +0100

    Oprava pro Swagger

--- app/utilities/swagger/documentationClass/Swagger_BASE64_FILE.java
+++ app/utilities/swagger/documentationClass/Swagger_BASE64_FILE.java
@@ -4,7 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description = "Json Model for getting Blocko_Block Filter List",
-          value = "Blocko_Block_Filter")
+          value = "Base64_File")
 public class Swagger_BASE64_FILE {
 
     @ApiModelProperty(required = false, value = "The file is encoded in base64. If it is null - it is a command to delete a file")
