452f5c180ea80304710c4ff8216eb310849b25fd
Tomas Zaruba <tomas.zaruba@byzance.cz>
2017-03-15 23:27:47 +0100

    Refaktoring Controlleru serverů

---
+++ app/controllers/Controller_ExternalServer.java
@@ -0,0 +1,237 @@
+package controllers;
+
+import io.swagger.annotations.*;
+import models.Model_HomerServer;
+import play.data.Form;
+import play.libs.Json;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Security;
+import utilities.enums.CLoud_Homer_Server_Type;
+import utilities.loggy.Loggy;
+import utilities.login_entities.Secured_API;
+import utilities.response.GlobalResult;
+import utilities.response.response_objects.Result_JsonValueMissing;
+import utilities.response.response_objects.Result_NotFound;
+import utilities.response.response_objects.Result_PermissionRequired;
+import utilities.response.response_objects.Result_ok;
+import utilities.swagger.documentationClass.Swagger_Cloud_Homer_Server_New;
+
+import java.util.List;
+
+@Security.Authenticated(Secured_API.class)
+public class Controller_ExternalServer extends Controller {
+
+    static play.Logger.ALogger logger = play.Logger.of("Loggy");
+
+    @ApiOperation(value = "Create new Blocko Server",
+            tags = {"External Server"},
+            notes = "Create new Gate for Blocko Server",
+            produces = "application/json",
+            protocols = "https",
+            code = 201,
+            extensions = {
+                    @Extension( name = "permission_description", properties = {
+                            @ExtensionProperty(name = "Cloud_Homer_Server.create_permission", value = Model_HomerServer.create_permission_docs ),
+                    }),
+                    @Extension( name = "permission_required", properties = {
+                            @ExtensionProperty(name = "Cloud_Blocko_Server.create_permission", value = "true"),
+                            @ExtensionProperty(name = "Static Permission key", value =  "Cloud_Homer_Server_creat" )
+                    })
+            }
+    )
+    @ApiImplicitParams(
+            {
+                    @ApiImplicitParam(
+                            name = "body",
+                            dataType = "utilities.swagger.documentationClass.Swagger_Cloud_Homer_Server_New",
+                            required = true,
+                            paramType = "body",
+                            value = "Contains Json with values"
+                    )
+            }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created",    response = Model_HomerServer.class),
+            @ApiResponse(code = 400, message = "Some Json value Missing", response = Result_JsonValueMissing.class),
+            @ApiResponse(code = 403, message = "Need required permission",response = Result_PermissionRequired.class),
+            @ApiResponse(code = 500, message = "Server side Error")
+    })
+    @BodyParser.Of(BodyParser.Json.class)
+    public Result create_Homer_Server(){
+        try{
+
+            // Zpracování Json
+            final Form<Swagger_Cloud_Homer_Server_New> form = Form.form(Swagger_Cloud_Homer_Server_New.class).bindFromRequest();
+            if(form.hasErrors()) {return GlobalResult.formExcepting(form.errorsAsJson());}
+            Swagger_Cloud_Homer_Server_New help = form.get();
+
+            // Vytvoření objektu
+            Model_HomerServer server = new Model_HomerServer();
+            server.personal_server_name = help.personal_server_name;
+            server.server_type = CLoud_Homer_Server_Type.public_server;
+
+            // Kontrola oprávnění
+            if(!server.create_permission()) return GlobalResult.forbidden_Permission();
+
+            // Uložení objektu
+            server.save();
+
+            // Vrácení objektu
+            return GlobalResult.created(Json.toJson(server));
+
+        } catch (Exception e) {
+            return Loggy.result_internalServerError(e, request());
+        }
+    }
+
+    @ApiOperation(value = "edit Compilation Server",
+            tags = {"External Server"},
+            notes = "Edit basic information Compilation Server",
+            produces = "application/json",
+            protocols = "https",
+            code = 200,
+            extensions = {
+                    @Extension( name = "permission_required", properties = {
+                            @ExtensionProperty(name = "Cloud_Homer_Server.edit_permission", value = "true"),
+                            @ExtensionProperty(name = "Static Permission key", value =  "Cloud_Homer_Server_edit" )
+                    })
+            }
+    )
+    @ApiImplicitParams(
+            {
+                    @ApiImplicitParam(
+                            name = "body",
+                            dataType = "utilities.swagger.documentationClass.Swagger_Cloud_Homer_Server_New",
+                            required = true,
+                            paramType = "body",
+                            value = "Contains Json with values"
+                    )
+            }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Updated successfully",    response = Model_HomerServer.class),
+            @ApiResponse(code = 400, message = "Object not found",        response = Result_NotFound.class),
+            @ApiResponse(code = 400, message = "Some Json value Missing", response = Result_JsonValueMissing.class),
+            @ApiResponse(code = 403, message = "Need required permission",response = Result_PermissionRequired.class),
+            @ApiResponse(code = 500, message = "Server side Error")
+    })
+    @BodyParser.Of(BodyParser.Json.class)
+    public Result edit_Homer_Server(@ApiParam(value = "server_id ", required = true) String server_id ){
+        try{
+
+            // Zpracování Json
+            final Form<Swagger_Cloud_Homer_Server_New> form = Form.form(Swagger_Cloud_Homer_Server_New.class).bindFromRequest();
+            if(form.hasErrors()) {return GlobalResult.formExcepting(form.errorsAsJson());}
+            Swagger_Cloud_Homer_Server_New help = form.get();
+
+            // Kontrola objektu
+            Model_HomerServer server = Model_HomerServer.find.byId(server_id);
+            if (server == null) return GlobalResult.notFoundObject("Cloud_Blocko_Server server_id not found");
+
+            // Kontrola oprávnění
+            if(!server.edit_permission()) return GlobalResult.forbidden_Permission();
+
+            // Úprava objektu
+            server.personal_server_name = help.personal_server_name;
+
+            server.mqtt_port = help.mqtt_port;
+            server.mqtt_password = help.mqtt_password;
+            server.mqtt_username = help.mqtt_username;
+
+            server.grid_port = help.grid_port;
+
+            server.webView_port = help.webView_port;
+
+            server.server_url = help.server_url;
+
+            // Uložení objektu
+            server.update();
+
+
+            // Vrácení objektu
+            return GlobalResult.result_ok(Json.toJson(server));
+
+        } catch (Exception e) {
+            return Loggy.result_internalServerError(e, request());
+        }
+    }
+
+    @ApiOperation(value = "get all Blocko Servers",
+            tags = {"External Server"},
+            notes = "get all Blocko Servers",
+            produces = "application/json",
+            consumes = "text/html",
+            protocols = "https",
+            code = 200,
+            extensions = {
+                    @Extension( name = "permission_description", properties = {
+                            @ExtensionProperty(name = "Cloud_Homer_Server.read_permission", value = Model_HomerServer.read_permission_docs ),
+                    }),
+                    @Extension( name = "permission_required", properties = {
+                            @ExtensionProperty(name = "Cloud_Homer_Server.read_permission", value = "true"),
+                            @ExtensionProperty(name = "Static Permission key", value =  "Cloud_Homer_Server_read")
+                    })
+            }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok Result",      response = Model_HomerServer.class, responseContainer = "List "),
+            @ApiResponse(code = 403, message = "Need required permission",response = Result_PermissionRequired.class),
+            @ApiResponse(code = 500, message = "Server side Error")
+    })
+    public Result get_All_Homer_Server(){
+        try{
+
+            // Získání seznamu
+            List<Model_HomerServer> servers = Model_HomerServer.find.all();
+
+            // Vrácení seznamu
+            return GlobalResult.result_ok(Json.toJson(servers));
+
+        } catch (Exception e) {
+            return Loggy.result_internalServerError(e, request());
+        }
+    }
+
+    @ApiOperation(value = "remove Compilation Servers",
+            tags = {"External Server"},
+            notes = "remove Compilation Servers",
+            produces = "application/json",
+            protocols = "https",
+            code = 200,
+            extensions = {
+                    @Extension( name = "permission_required", properties = {
+                            @ExtensionProperty(name = "Cloud_Homer_Server.delete_permission", value = "true"),
+                            @ExtensionProperty(name = "Static Permission key", value =  "Cloud_Homer_Server_delete")
+                    })
+            }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok Result",               response = Result_ok.class),
+            @ApiResponse(code = 400, message = "Object not found",        response = Result_NotFound.class),
+            @ApiResponse(code = 403, message = "Need required permission",response = Result_PermissionRequired.class),
+            @ApiResponse(code = 500, message = "Server side Error")
+    })
+    public Result delete_Homer_Server(@ApiParam(value = "server_id ", required = true)  String server_id ){
+        try{
+
+            // Kontrola objektu
+            Model_HomerServer server = Model_HomerServer.find.byId(server_id);
+            if (server == null) return GlobalResult.notFoundObject("Cloud_Compilation_Server server_id not found");
+
+            // Kontrola oprávnění
+            if(!server.delete_permission()) return GlobalResult.forbidden_Permission();
+
+            // Smzání objektu
+            server.delete();
+
+            // Vrácení potvrzení
+            return GlobalResult.result_ok();
+
+        } catch (Exception e) {
+            return Loggy.result_internalServerError(e, request());
+        }
+    }
+
+}
