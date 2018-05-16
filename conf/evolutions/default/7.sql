e9191e1dfddf0519add0af1440b87b896adadbff
Tomas Záruba <tomas.zaruba@byzance.cz>
2018-05-16 14:50:31 +0200

    HotFix for GSM. Statistics

--- conf/evolutions/default/7.sql
+++ conf/evolutions/default/7.sql
@@ -2,11 +2,16 @@
 
 ALTER TABLE gsm
   ADD column msi_number bigint,
-  DROP column msinumber;
+  ADD column json_bootloader_core_configuration TEXT,
+  ADD column blocked boolean default TRUE not null,
+  DROP column msinumber,
+  DROP column private_additional_information;
 
 
 # --- !Downs
 
 ALTER TABLE gsm
   ADD column msinumber varchar(255),
-  DROP column msi_number;
+  ADD column private_additional_information TEXT,
+  DROP column msi_number,
+  DROP column blocked;
