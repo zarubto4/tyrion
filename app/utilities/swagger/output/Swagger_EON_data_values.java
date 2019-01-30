206d7877a5809fd29e1a7faf05337b443a45a43f
Alexey Shvachka <Shvachka.Alexey@gmail.com>
2019-01-30 23:29:45 +0100

    fix

--- app/utilities/swagger/output/Swagger_EON_data_values.java
+++ app/utilities/swagger/output/Swagger_EON_data_values.java
@@ -2,12 +2,13 @@ package utilities.swagger.output;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import utilities.swagger.output.filter_results._Swagger_Abstract_Default;
 
 import java.util.Date;
 
 @ApiModel(description = "Json Model for EON",
         value = "EON Data values")
-public class Swagger_EON_data_values {
+public class Swagger_EON_data_values extends _Swagger_Abstract_Default {
     @ApiModelProperty(required = true, readOnly = true )
     public String hardware;
     @ApiModelProperty(required = true, readOnly = true )
